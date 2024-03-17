37ccbe0252e548da1229b04acb8de5bdc15aafff
Asantos <161307304+rnelsantos@users.noreply.github.com>
2024-03-17 15:20:37 +0800

    auto clearout GRid when entering a new Grid size

--- Grid_javascript.js
+++ Grid_javascript.js
@@ -3,7 +3,8 @@ const button = document.getElementById('button');
 //event listener for button
 button.addEventListener('click', Click);
 
-
+//select grid-container/content div;
+const contentDiv = document.querySelector(".content");
 const container = document.querySelector(".grid-Container");
 
 const cellDiv = document.createElement('div');
@@ -22,11 +23,12 @@ function Click(){
 }
 
 function getNumberInput(){
+
     while (true) { // Loop until valid input
         const userInput = prompt("Enter Grid Dimension (1-50): ");
         const numberRegex = /^\d+$/; // Regex for positive integers
     
-        if (numberRegex.test(userInput)) {
+        if (numberRegex.test(userInput)&&userInput<50) {
           return Number(userInput); // Convert to number and return
         } else {
           alert("Please enter a valid number(Max=50).");
@@ -34,10 +36,14 @@ function getNumberInput(){
       }
     }
    
-
-
-
 function generateGrid(dim){
+  
+  while (container.firstChild) {// clear out old GRID
+    while(rowDiv.firstChild){rowDiv.removeChild(rowDiv.firstChild);}
+    container.removeChild(container.firstChild);
+  }
+
+
     for (let i=0; i<dim; i++){
         const newCell = cellDiv.cloneNode(true);
         rowDiv.appendChild(newCell);
